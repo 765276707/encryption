719a4cbc7fec11f72fe810000a87fbc82eb54300
Pristine <1806606800@qq.com>
2022-05-13 16:27:47 +0800

    更新框架的配置类

--- encryption-spring-boot-sample/src/main/java/com/github/xzb617/encyrption/sample/config/EncryptionConfig.java
+++ encryption-spring-boot-sample/src/main/java/com/github/xzb617/encyrption/sample/config/EncryptionConfig.java
@@ -12,7 +12,23 @@ import org.springframework.context.annotation.Configuration;
  * @date 2022/5/7 14:46
  * @description:
  */
+//@Configuration
 public class EncryptionConfig extends EncryptionConfigure {
 
+    @Override
+    public void configure(Registry registry) {
+        // 注册自定义加密器
+        registry.registerArgumentEncryptor(new Rc4ArgumentEncryptor());
+    }
+
+    @Override
+    public void configure(Configurator configurator) {
+        // 开启调试模式
+        configurator.enableShowDetails()
+                // 禁止展示头信息
+                .disableShowDetailsHeader()
+                // 设置调试模式追踪的编号，在请求头中，由客户端传入，若未传则服务端会自动生成一个随机编号
+                .setRequestIdKey("X-Request-Id");
+    }
 
 }
